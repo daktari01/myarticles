d3442484b55d47ec4067325f3025bbe6a2b53dbe
James Dindi <dindijjames@gmail.com>
2018-08-27 01:05:40 +0300

    PUT Update an article

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -22,6 +22,15 @@ module Api
                 article.destroy
                 render json: {status: 'SUCCESS', message:'Article deleted', data:article}, status: :ok
             end
+            def update
+                article = Article.find(params[:id])
+                if article.update_attributes(article_params)
+                    render json: {status: 'SUCCESS', message: 'Updated article', data:article}, status: :ok
+                else
+                    render json: {status: 'ERROR', message:'Article not updated', data:article.errors}, status: :unprocessable_entry
+                end
+            end
+            
             private def article_params
                 params.permit(:title, :body)
             end
